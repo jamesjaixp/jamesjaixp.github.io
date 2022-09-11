8bfa767d14c14b442d0d9ecfbc1224a907158ea0
James L <47505791+jamesjaixp@users.noreply.github.com>
2022-09-10 21:40:46 -0500

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy
+
+
+# In[6]:
+
+
+data = pd.read_csv('data-1.csv')
+
+
+# In[7]:
+
+
+print(data)
+
+
+# In[ ]:
+
+
+
+
